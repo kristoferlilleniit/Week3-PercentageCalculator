de51d31d95a11af9b174ba72a7be557bb28ff454
Opilane TTHK <opilane@tthk.ee>
2020-09-14 14:57:17 +0300

    First commit

---
+++ PercentageCalculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PercentageCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Maximum;
+            int UserScore;
+            double Result;
+
+            Console.WriteLine("What's the maximum score?");
+            Maximum = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("What's your score?");
+            UserScore = Int32.Parse(Console.ReadLine());
+            Result = (UserScore * 100) / Maximum;
+
+            //Console.WriteLine($"You scored {Result}%.");
+
+            if (Result >= 90)
+            {
+                Console.WriteLine($"You scored {Result}%. Your grade is 5.");
+            }
+            else if (Result >= 80)
+            {
+                Console.WriteLine($"You scored {Result}%. Your grade is 4.");
+            }else if (Result >= 61)
+            {
+                Console.WriteLine($"You scored {Result}%. Your grade is 3.");
+            }else
+            {
+                Console.WriteLine($"You scored {Result}%. Your grade is 2.");
+            }
+        }
+    }
+}
